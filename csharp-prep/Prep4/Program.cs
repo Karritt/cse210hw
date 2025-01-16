7ccd4349eba5cad9f947d874ae5977b8d7be32cc
Karritt <45860943+Karritt@users.noreply.github.com>
2025-01-16 15:32:53 -0700

    Solve Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userInput;
+        List<int> numbers = new List<int>();
+        do {
+            Console.WriteLine("Enter a Number. ");
+            userInput = int.Parse(Console.ReadLine());
+            numbers.Add(userInput);
+        } while (userInput != 0);
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {(numbers.Sum() * 1.0)/numbers.Count}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
