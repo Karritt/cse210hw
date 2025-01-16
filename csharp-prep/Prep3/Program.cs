1953baf6aa838be6ddedd52c094ef6dd1ab845c1
Karritt <45860943+Karritt@users.noreply.github.com>
2025-01-16 15:18:41 -0700

    Solve Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int guessMe = randomGenerator.Next(1,100);
+        int guess = 0;
+        do
+        {
+            Console.Write("Guess a number between 1 and 100 >");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < guessMe)
+            {
+                Console.WriteLine("It's higher!");
+            } else if (guess > guessMe) 
+            {
+                Console.WriteLine("It's lower!");
+            }
+        } while (guess != guessMe);
+        Console.WriteLine("You Win!");
     }
 }
