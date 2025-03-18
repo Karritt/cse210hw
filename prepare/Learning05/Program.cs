09eee08e442eb3cdb10431322fbbc1274eaea862
Karritt <45860943+Karritt@users.noreply.github.com>
2025-03-18 11:24:08 -0600

    solve Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle(5, "Red"));
+        shapes.Add(new Rectangle(4, 5, "Blue"));
+        shapes.Add(new Square(4, "Green"));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Area of {shape.GetColor()} shape is {shape.GetArea()}");
+        }
     }
 }
